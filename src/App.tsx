8e6c784811fbefde0961d87c5caae4d2cc2f6e64
hungdv <danghungtb26@gmail.com>
2022-08-25 12:52:06 +0700

    config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const variable: string = 'hello'
+
+const data: any[] = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany',
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico',
+  },
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        {data.map(element => {
+          return (
+            <tr key={element.company}>
+              <td>{element.company}</td>
+              <td>{element.contact}</td>
+              <td>{element.country}</td>
+            </tr>
+          )
+        })}
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
